c3b113120397e818ba85c6ad2e40dcb9abe5b3f5
shiveshcshukla <shivesh.shukla@primelab.io>
2022-05-20 20:13:29 +0530

    dashboard created from scratch

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,19 +1,101 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
 import { ReactStateDeclaration } from "@uirouter/react";
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import SimpleCard from "./components/Dashboard/cards";
+import { Card } from "@material-ui/core";
+import UserCard from "./components/Dashboard/userCards";
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            flexGrow: 1,
+            padding:5,
+        },
+        paper: {
+            padding: theme.spacing(2),
+            //   textAlign: 'center',
+            background: "F1EEE9",
+            color: theme.palette.text.secondary,
+        },
+    }),
+);
 
 export function Dashboard() {
-  return <Grid className="p-3">Dashboard</Grid>;
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <Grid container spacing={2}>
+                <Grid item xs={6} >
+                    <Card>
+                        <Paper variant="outlined" square className={classes.paper}>
+                            Appointments
+                        </Paper>
+
+                        <Grid container spacing={2} className={classes.root}>
+                            <Grid item xs={4}>
+                                <SimpleCard title="In Progress" appointments={5} />
+                            </Grid>
+                            <Grid item xs={4}>
+                                <SimpleCard title="Upcoming" appointments={10} />
+                            </Grid>
+                            <Grid item xs={4}>
+                                <SimpleCard title="Rejected" appointments={3} />
+                            </Grid>
+                        </Grid>
+                    </Card>
+
+                </Grid>
+                <Grid item xs={6}>
+                    <Card>
+                        <Paper variant="outlined" square className={classes.paper}>
+                            Users
+                        </Paper>
+
+                        <Grid container spacing={2} className={classes.root}>
+                            <Grid item xs={6} >
+                                <UserCard title="Doctors" appointments={5} />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <UserCard title="Patients" appointments={10} />
+                            </Grid>
+                            {/* <Grid item xs={4}>
+                                <SimpleCard title="Rejected" appointments={3} />
+                            </Grid> */}
+                        </Grid>
+                    </Card>
+                </Grid>
+
+                {/* <Paper className={classes.paper}>Recent Upcoming Appointmetns</Paper> */}
+                <Grid item xs={12}><Paper className={classes.paper}>Recent Upcoming Appointmetns</Paper></Grid>
+                <Grid item xs={3}>
+                    <Paper className={classes.paper}>Appointment 1</Paper>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper className={classes.paper}>Appointment 2</Paper>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper className={classes.paper}>Appointment 3</Paper>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper className={classes.paper}>Appointment 4</Paper>
+                </Grid>
+                
+
+            </Grid>
+        </div>
+    )
 }
 
 export const states: ReactStateDeclaration[] = [
-  {
-    url: "/dashboard",
-    name: "dashboard",
-    data: {
-      title: "Dashboard",
-      loggedIn: true,
+    {
+        url: "/dashboard",
+        name: "dashboard",
+        data: {
+            title: "Dashboard",
+            loggedIn: true,
+        },
+        component: Dashboard,
     },
-    component: Dashboard,
-  },
 ];
---
+++ src/components/Dashboard/userCards.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+// import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    // minWidth: 155,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+export default function UserCard({title,appointments}) {
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography variant="h6" component="h2">
+          {title}
+          <hr></hr>
+        </Typography>
+        <Typography component="h6">
+          Total :
+        </Typography>
+        <Typography component="h6">
+          Corporate :
+        </Typography>
+        <Typography component="h6">
+          Pending :
+        </Typography>
+        {/* <Typography className={classes.pos} color="textSecondary">
+        </Typography> */}
+      </CardContent>
+    </Card>
+  );
+}
